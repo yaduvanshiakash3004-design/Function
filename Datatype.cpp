d1bbdb68984e6605a69c59a76ae187065e3086c0
yaduvanshiakash3004-design <yaduvanshiakash3004@gmail.com>
2026-02-12 23:45:58 +0530

    Notes

---
+++ Datatype.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // long long int a=198346;
+    // cout<<a*a<<endl;
+    // double b=768452.7869;
+    // cout<<b*6.4<<endl;
+    char a='l';
+    cout<<sizeof(a)<<endl;
+
+    return 0;
+}
