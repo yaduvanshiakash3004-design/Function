0e507153e4152e45ef627bd5f7d57b19ba2711d5
yaduvanshiakash3004-design <yaduvanshiakash3004@gmail.com>
2026-02-17 22:50:45 +0530

    Notes

---
+++ Day 1/Leetcode/Array/Print_array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5];
+
+    // input
+    for(int i=0;i<5;i++)
+    {
+        cin>>arr[i];
+    }
+
+    // print
+    for(int i=0;i<5;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
+
