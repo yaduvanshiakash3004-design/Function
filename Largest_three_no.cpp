7bd9deb5e6479d8ce2a04826a36771ac4c068ebe
yaduvanshiakash3004-design <yaduvanshiakash3004@gmail.com>
2026-02-20 23:38:34 +0530

    Notes

---
+++ Largest_three_no.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if(a >= b && a >= c)
+        cout << "Largest = " << a;
+    else if(b >= a && b >= c)
+        cout << "Largest = " << b;
+    else
+        cout << "Largest = " << c;
+
+    return 0;
+}
