1cc7876ada4ded39f578c5195f76f8ce8314c7aa
yaduvanshiakash3004-design <yaduvanshiakash3004@gmail.com>
2026-03-04 20:58:49 +0530

    Notes

---
+++ Day 1/Leetcode/Second_Largest_Element_in_Array.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int largest = arr[0];
+    int secondLargest = INT_MIN;
+
+    for(int i = 1; i < n; i++) {
+
+        if(arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+
+        else if(arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+
+    }
+
+    cout << secondLargest;
+
+    return 0;
+}
