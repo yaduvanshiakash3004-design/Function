ec41a3873c390ad9a67964fd7f9d3ae1e27732ee
yaduvanshiakash3004-design <yaduvanshiakash3004@gmail.com>
2026-03-04 21:02:18 +0530

    Notes

---
+++ Day 1/Leetcode/Two_sum.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int target;
+    cin >> target;
+
+    sort(arr, arr + n);
+
+    int left = 0;
+    int right = n - 1;
+
+    while(left < right) {
+
+        int sum = arr[left] + arr[right];
+
+        if(sum == target) {
+            cout << arr[left] << " " << arr[right];
+            break;
+        }
+
+        else if(sum < target) {
+            left++;
+        }
+
+        else {
+            right--;
+        }
+    }
+
+    return 0;
+}
