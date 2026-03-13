91a5bc78323cf9e51be4a6fd31b42d00bce3bc0a
yaduvanshiakash3004-design <yaduvanshiakash3004@gmail.com>
2026-03-13 22:51:46 +0530

    Notes

---
+++ Day 1/2_matrix_multiplication.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>> a(n,vector<int>(m));
+    vector<vector<int>> b(n,vector<int>(m));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>a[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>b[i][j];
+        }
+    }
+    vector<vector<int>> c(n,vector<int>(m));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            c[i][j]=a[i][j]*b[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<c[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
