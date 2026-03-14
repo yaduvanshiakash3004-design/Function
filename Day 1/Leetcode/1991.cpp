4958df80adfe70d1488013b0714eaf03ff74fafc
yaduvanshiakash3004-design <yaduvanshiakash3004@gmail.com>
2026-03-14 19:12:30 +0530

    Notes

---
+++ Day 1/Leetcode/1991.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        int totalSum = 0;
+
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        int leftSum = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int rightSum = totalSum - leftSum - nums[i];
+
+            if (leftSum == rightSum) {
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> nums = {2,3,-1,8,4};
+
+    int result = sol.findMiddleIndex(nums);
+
+    cout << "Middle Index: " << result << endl;
+
+    return 0;
+}
