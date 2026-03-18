3c348931fe0957cc6a37f7657dedddb2ff496b70
yaduvanshiakash3004-design <yaduvanshiakash3004@gmail.com>
2026-03-18 21:11:54 +0530

    Notes

---
+++ Assignment_5/Que_8.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int matrix[n][n];
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << matrix[j][i] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
