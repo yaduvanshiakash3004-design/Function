88778a57991eb0f80769a07ba45cd4c10d09b2d5
yaduvanshiakash3004-design <yaduvanshiakash3004@gmail.com>
2026-03-22 18:40:21 +0530

    Notes

---
+++ Assignment_5/Que_19.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int arr[100][100];
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+
+            if(j < i) {
+                cout << "0 ";
+            }
+            else {
+                cout << arr[i][j] << " ";
+            }
+
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
