d75d37c9e00a0841da8c5bbdd5de4ee7de10e8e9
yaduvanshiakash3004-design <yaduvanshiakash3004@gmail.com>
2026-03-23 22:01:35 +0530

    Notes

---
+++ Assignment_5/Assignment_6.cpp/Que_5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int count = 1;
+
+    for (int i = 0; i < s.length(); i++) {
+
+        if (i < s.length() - 1 && s[i] == s[i + 1]) {
+            count++;
+        } 
+        else {
+           
+            cout << s[i];
+
+            if (count > 1) {
+                cout << count;
+            }
+
+            count = 1;
+        }
+    }
+
+    return 0;
+}
