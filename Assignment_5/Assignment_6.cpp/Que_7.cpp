486534a5ca5cbbc0aee62eadc038f130cb24738f
yaduvanshiakash3004-design <yaduvanshiakash3004@gmail.com>
2026-03-24 23:46:11 +0530

    Notes

---
+++ Assignment_5/Assignment_6.cpp/Que_7.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int count = 0, maxLen = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+
+        if (s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u') {
+            count++;
+            if (count > maxLen) {
+                maxLen = count;
+            }
+        } else {
+            count = 0; 
+        }
+    }
+
+    cout << maxLen;
+
+    return 0;
+}
