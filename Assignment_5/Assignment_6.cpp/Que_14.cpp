359fcd33a4ce196fa7b2c6feec1694643afe46d2
yaduvanshiakash3004-design <yaduvanshiakash3004@gmail.com>
2026-03-26 23:45:57 +0530

    Notes

---
+++ Assignment_5/Assignment_6.cpp/Que_14.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int freq[256] = {0};
+
+    // Count frequency
+    for (char ch : s) {
+        freq[ch]++;
+    }
+
+    char ans;
+    int maxFreq = 0;
+
+    // Find max frequency character
+    for (char ch : s) {
+        if (freq[ch] > maxFreq) {
+            maxFreq = freq[ch];
+            ans = ch;
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
