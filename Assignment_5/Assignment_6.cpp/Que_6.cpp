5e56b63c9ce91512b3c41116ccbe2e8bc4b7ea3e
yaduvanshiakash3004-design <yaduvanshiakash3004@gmail.com>
2026-03-29 23:11:44 +0400

    Notes

--- Assignment_5/Assignment_6.cpp/Que_6.cpp
+++ Assignment_5/Assignment_6.cpp/Que_6.cpp
@@ -5,7 +5,8 @@ int main() {
     string s;
     cin >> s;
 
-    for (int i = 0; i < s.length() - 1; i++) {
+    for (int i = 0; i < s.length() - 1; i++) 
+    {
         cout << s[i];
 
         int diff = s[i + 1] - s[i];
