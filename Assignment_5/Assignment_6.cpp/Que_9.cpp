aad5cc9f624019955fad17477beba169370fb4a6
yaduvanshiakash3004-design <yaduvanshiakash3004@gmail.com>
2026-03-30 20:57:40 +0530

    Notes

--- Assignment_5/Assignment_6.cpp/Que_9.cpp
+++ Assignment_5/Assignment_6.cpp/Que_9.cpp
@@ -21,7 +21,7 @@ int main() {
             cin >> arr[i];
         }
 
-        sort(arr.begin(), arr.end(), compare);
+        sort(arr.begin() , arr.end(), compare);
 
         for (int i = 0; i < n; i++)
          {
