8759eb3f37cd940778a2c3d70541967ee7eed8e5
yaduvanshiakash3004-design <yaduvanshiakash3004@gmail.com>
2026-04-04 23:08:21 +0530

    Notes

--- Assignment_5/Que_10.cpp
+++ Assignment_5/Que_10.cpp
@@ -8,7 +8,8 @@ int main() {
     int matrix[n][n];
 
     for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
+        for(int j = 0; j < n; j++)
+         {
             cin >> matrix[i][j];
         }
     }
