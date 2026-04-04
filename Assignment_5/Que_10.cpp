0de1676a46d0435ed90387c40f71386a2d4e6dca
yaduvanshiakash3004-design <yaduvanshiakash3004@gmail.com>
2026-04-04 23:06:57 +0530

    Notes

--- Assignment_5/Que_10.cpp
+++ Assignment_5/Que_10.cpp
@@ -15,7 +15,8 @@ int main() {
 
     bool isLowerTriangular = true;
 
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) 
+    {
         for(int j = i + 1; j < n; j++) 
         {
             if(matrix[i][j] != 0) 
