c6fc36e172562cb3230aace0ea40f26be3642a09
yaduvanshiakash3004-design <yaduvanshiakash3004@gmail.com>
2026-04-05 22:14:39 +0530

    Notes

--- Assignment_5/Que_6.cpp
+++ Assignment_5/Que_6.cpp
@@ -14,7 +14,8 @@ int main() {
     }
 
     for(int j = n-1; j >= 0; j--){
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++)
+        {
             cout << arr[i][j] << " ";
         }
         cout << endl;
