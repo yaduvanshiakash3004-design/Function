b5f6204b1e650fc0601175f767f4e75b37959493
yaduvanshiakash3004-design <yaduvanshiakash3004@gmail.com>
2026-04-06 21:51:44 +0530

    Notes

--- Assignment_5/Que_6.cpp
+++ Assignment_5/Que_6.cpp
@@ -8,7 +8,8 @@ int main() {
     int arr[1000][1000];
 
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
+        for(int j = 0; j < n; j++)
+        {
             cin >> arr[i][j];
         }
     }
